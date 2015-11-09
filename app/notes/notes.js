17dfa4cf990737f67df877a262e654533079bd09
Matt Caskey <mcaskey@e-tcc.com>
2015-11-09 16:55:37 -0500

    Add Note Form

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -11,7 +11,7 @@
 
     .state('notes', {
       url: '/notes',
-      template: '<h1>Notely</h1><p>{{ message }}</p><div ui-view></div>',
+      templateUrl: 'notes/notes.html',
       controller: NotesController
     })
 
@@ -21,8 +21,8 @@
     });
   }
 
-  NotesController['$inject'] = ['$scope'];
-  function NotesController($scope) {
-    $scope.message = 'I <3 Angular.';
+  NotesController['$inject'] = ['$state'];
+  function NotesController($state) {
+    $state.go('notes.form');
   }
 })();
